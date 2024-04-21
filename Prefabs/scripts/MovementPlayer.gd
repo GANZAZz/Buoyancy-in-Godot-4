9d9757d32d0b7e4761e3be3ed9f58a9b737b7cb7
ILYA_MATEEV <86591199+GANZAZz@users.noreply.github.com>
2024-04-21 17:07:32 +0800

    Test water shader with isometric ocean godot 4.2.2

---
+++ Prefabs/scripts/MovementPlayer.gd
@@ -0,0 +1,46 @@
+extends RigidBody3D
+
+@export var float_force := 1.0
+@export var water_drag := 0.05
+@export var water_angular_drag := 0.05
+
+@onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var water = get_node('/root/Main/Water')
+
+@onready var probes = $ProbeContainer.get_children()
+
+var submerged := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func Move():
+
+	var basis = get_global_transform().basis
+	
+	if Input.is_action_pressed("ui_up"):
+		apply_central_force(-basis.z * 18)
+	if Input.is_action_pressed("ui_left"):
+		apply_torque(Vector3(0, 15, 0))
+	if Input.is_action_pressed("ui_right"):
+		apply_torque(Vector3(0, -15, 0))
+
+func _physics_process(delta):
+	submerged = false
+	for p in probes:
+		var depth = water.get_height(p.global_position) - p.global_position.y 
+		if depth > 0:
+			submerged = true
+			apply_force(Vector3.UP * float_force * gravity * depth, p.global_position - global_position)
+	Move()
+
+func _integrate_forces(state: PhysicsDirectBodyState3D):
+	if submerged:
+		state.linear_velocity *=  1 - water_drag
+		state.angular_velocity *= 1 - water_angular_drag 
